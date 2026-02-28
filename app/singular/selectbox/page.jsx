1dfe7fa46aba43ff9ae7c2e7c898314eeef04d34
Hoang Nam <dhnam.dang1997@gmail.com>
2026-02-28 13:32:59 +0700

    CustomSelectBox

---
+++ app/singular/selectbox/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import CustomSelectBox from "../selectbox/components/CustomSelectBox";
+const SelectBoxPage = () => {
+  return (
+    <div className="bg-white rounded-lg mx-4 p-4">
+      <h1>Đây là page Selectbox</h1>
+      <br />
+      <br />
+      <CustomSelectBox></CustomSelectBox>
+    </div>
+  );
+};
+
+export default SelectBoxPage;
